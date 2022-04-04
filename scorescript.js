5aaf8728d5118f076ef3fc697a5983219128a641
Haley Seymour <haleycseymour@comcast.net>
2022-04-04 00:45:42 -0700

    updating scoreboard; incomplete

--- scorescript.js
+++ scorescript.js
@@ -1,8 +1,51 @@
-var clear = document.querySelector("#clear");
-var list = document.querySelector("#scoreboard");
+// var clear = document.querySelector("#clear");
+// var scoreboard = document.querySelector("#scoreboard")
 
-clear.addEventListener("click", function() {
-    console.log("hello")
-        // working; need to clear ol when clicked
+// clear.addEventListener("click", function() {
+//     console.log("hello")
+//     scoreboard.textContent = '';
+// })
 
-})
+// addToScoreboard();
+
+// function addToScoreboard () {
+//     var lastInitials = localStorage.getItem("initials");
+//     var lastScore = localStorage.getItem("score");
+//     var scoreSet = `${lastInitials} - ${lastScore}`
+
+// console.log(lastInitials);
+// console.log(lastScore);
+// console.log(scoreSet);
+
+// var newScore = document.createElement("li");
+// newScore.textContent = scoreSet;
+// scoreboard.appendChild(newScore);
+
+// }
+
+var scores = [
+    {
+        user: 'Haley', 
+        score: 12
+    },
+    {
+        user: 'Toby',
+        score: 45
+    },
+    {
+        user: 'dad', 
+        score: 77
+    },
+    {
+        user: 'brenda',
+        score: 5
+    }
+    
+];
+console.log(scores);
+
+var sorted = scores.sort(function(a,b) {
+    return b.score - a.score;
+});
+
+console.log(sorted)
--- script.js
+++ script.js
@@ -60,68 +60,82 @@ function startTrivia() {
     questionPage.setAttribute("style", "display: flex");
     // start timer
     startTimer();
-    // show first question
+    // show  question
     nextQuestion();
 
-    // listening for anywhere in the pink
+    // listening for click anywhere in the ordered list
     answerEl.addEventListener("click", function(event) {
         // determine if correct or not 
         // run correct or incorrect function which includes: going to next screen; display correct or incorrect; adjusting clock
         event.preventDefault();
-        let userSelection = event.target.innerHTML;
-        console.log(userSelection);
-        let correctAnswer = "Incorrect";
-        if (userSelection == currentQuestion.answer) {
-            correctAnswer = "Correct!"
-            response.textContent = correctAnswer;
-            questionPage.appendChild(response);
-            console.log("YAY!")
-        } else {
-            correctAnswer = "Incorrect";
-            timeLeft = timeLeft-10;
-            response.textContent = correctAnswer;
-            questionPage.appendChild(response);
+        let userSelection = event.target;
+        let userSelectionText = event.target.innerHTML;
+        // if click was within a list element
+        if (userSelection.matches("li")) {
+            let correctAnswer = "Incorrect";
+            // if selected answer matches answer value from object
+            if (userSelectionText == currentQuestion.answer) {
+                // post to screen that it was correct
+                correctAnswer = "Correct!"
+                response.textContent = correctAnswer;
+                questionPage.appendChild(response);
+            // if selected answer does not match answer value from object
+            } else {
+                // post to screen that it was incorrect
+                correctAnswer = "Incorrect";
+                // subtract 10 seconds from clock
+                timeLeft = timeLeft-10;
+                response.textContent = correctAnswer;
+                questionPage.appendChild(response);
+            }
+            // format response and set timer for 2 seconds
+            response.setAttribute("style", "padding:15px 50px; text-align:right;")
+            setTimeout(function() {
+                response.textContent=''; 
+            }, 2000)
+            // on click, move to next question
+            nextQuestion();
         }
-        response.setAttribute("style", "padding:15px 50px; text-align:right;")
-        setTimeout(function() {
-            response.textContent=''
-        }, 2000)
-
-        nextQuestion();
     })
 }
 
+// create timer for 60 seconds; updating each second
+let timerInterval;
 let score;
 var timeLeft = 60;
 function startTimer () {
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         timeLeft--;
         timer.textContent = `Time: ${timeLeft}`
         if (timeLeft == 0) {
             clearInterval(timerInterval);
             score = timeLeft;
-            console.log(score)
-            endGame()
-            // end game
+            endGame();
         }
     }, 1000);
 }
 
-let index = 0;
-let currentQuestion = questions[index];
+// create function to move through questions
+let i = 0;
+let currentQuestion = questions[i];
 function nextQuestion() {
-    if (index === 5) {
+    // if on the last question
+    if (i === questions.length) {
+        // assign score of time on clock
         score = timeLeft; 
-        console.log(score);
+        // need to clear interval or score will turn to 0 when clock is out
+        clearInterval(timerInterval);
+        // run end game function
         endGame();
+        // otherwise, change text to the next question
     } else {
-        currentQuestion = questions[index];
+        currentQuestion = questions[i];
         question.textContent = currentQuestion.title;
         answerOne.textContent = currentQuestion.choices[0];
         answerTwo.textContent = currentQuestion.choices[1];
         answerThree.textContent = currentQuestion.choices[2];
         answerFour.textContent = currentQuestion.choices[3];
-        index++;
+        i++;
         }
     }
 
@@ -129,10 +143,78 @@ var header = document.querySelector("header");
 var resultsPage = document.querySelector("#resultspage");
 var finalScore = document.querySelector("#finalscore")
 
+// at end of game (clock runs out or all questions were completed)
 function endGame() {
+    // turn off question page and header
     questionPage.setAttribute("style", "display:none");
     header.setAttribute("style", "display:none");
+    // turn on result page
     resultsPage.setAttribute("style", "display:flex");
+    // update score to page
     finalScore.textContent = `Your final score is ${score}.`
 }
 
+var submitButton = document.querySelector("#submit");
+var initials = document.querySelector("#initials");
+var userScore;
+
+// on submit, create an object to store user's data
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    initials = initials.value;
+    userScore = {
+        initials: initials,
+        score: score,
+        scoreset: `${initials} : ${score}`,
+    }
+    // stringify to send to local storage
+    userScoreJson = JSON.stringify(userScore)
+    localStorage.setItem("userScore", userScoreJson), 
+    // turn off result page and turn on score page
+    resultsPage.setAttribute("style", "display:none");
+    scorePage.setAttribute("style", "display: flex");
+    // execute function to add user to scoreboard
+    addToScoreboard();
+})
+
+var highscoresButton = document.querySelector("#highscores");
+var scorePage = document.querySelector("#scorepage")
+
+// switch page correctly when selected
+highscoresButton.addEventListener("click", function() {
+    homePage.setAttribute("style", "display: none");
+    questionPage.setAttribute("style", "display: none");
+    scorePage.setAttribute("style", "display: flex");
+    header.setAttribute("style", "display:none");
+})
+
+var clear = document.querySelector("#clear");
+var scoreboard = document.querySelector("#scoreboard")
+
+clear.addEventListener("click", function() {
+    console.log("hello")
+    scoreboard.textContent = '';
+})
+
+// building scoreboard WIP
+function addToScoreboard () {
+    // pull from local storage and return as object
+    var savedUser = JSON.parse(localStorage.getItem("userScore"));
+    let savedScores = [];
+    // push object to array of savedScores
+    savedScores.push(savedUser);
+    if (savedScores != null) {
+        // if there are objects within array, sort by high score
+        var sortedScores = savedScores.sort(function(a,b) {
+            return b.score - a.score;
+        });
+        // post scores
+        for (i=0; i<sortedScores.length; i++) {
+            // add new post for each item in array
+            var user = sortedScores[i];
+            var newScore = document.createElement("li");
+            newScore.textContent = user.scoreset;
+            scoreboard.appendChild(newScore);
+        }
+    } 
+}
